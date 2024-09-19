7badacd8cc0669b1ad15122026185bc8bc6343b8
Alex6969420 <alexandruopreaca@gmail.com>
2024-09-19 09:34:43 -0400

    holy in made a program

--- firstprogram.py
+++ firstprogram.py
@@ -1,1 +1,8 @@
-print("hello world")
+#Basic Calculator
+
+print("hey Fam, welcome to my bussin calculator!")
+num1 = int(input("Enter a number: "))
+num2 = int(input("Enter another number: "))
+
+answer = num1 + num2
+print(answer)
